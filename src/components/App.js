b5a19e059588b89708a6b8db182cc4627d407f77
Allison Bohannon <akboh.github@gmail.com>
2022-07-24 08:33:37 -0700

    commit tree 749aea1d2de07c6489bcc1c5ee721e5671f6b0c4

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [url, setURL] = useState('')
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(response => response.json())
+            .then(data =>{
+                 setURL(data.message)
+                 setIsLoaded(true)
+                })
+    })
+
+    if (!isLoaded) return <p>Loading...</p>
+    return <img src={url} alt='A Random Dog'/>
+
+}
+
+export default App; 
